6c283d7b1571b9f4d15b071e079438382d034cab
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-08-27 11:59:27 -0600

    Updates

---
+++ precache-manifest.2caa00e62a9e1f1c2ec3444e3bedc518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346d3a245ed7a2171d891c494000a858",
+    "url": "/Landdox/index.html"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/Landdox/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5333f5ce38154a484606",
+    "url": "/Landdox/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/Landdox/static/js/2.ef63f5f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Landdox/static/js/2.ef63f5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5333f5ce38154a484606",
+    "url": "/Landdox/static/js/main.32d278a8.chunk.js"
+  },
+  {
+    "revision": "1a33935ef66b7be3b2c7",
+    "url": "/Landdox/static/js/runtime-main.19726b4e.js"
+  }
+]);

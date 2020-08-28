4884b917310bdf64bcb965b39b24216110bc66aa
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-08-27 18:00:48 -0600

    work on should render with mineral interests

--- src/EditTractOwnership/index.js
+++ src/EditTractOwnership/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Table, InputGroup, FormControl, Button } from 'react-bootstrap';
+import Icon from '../Icon/index.js';
 
-const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
+const EditTractOwnership = ({ value = [], onChange = () => {} }) => {
+  console.log(value);
   return (
-    <h2>
-      TODO
-    </h2>
+    <div>
+      <div>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Mineral Interest</th>
+              <th>NPRI</th>
+              <th>Lease</th>
+            </tr>
+          </thead>
+          <tbody>
+            {value.map((tractOwnerships) => (
+              <tr key={`mineralInterest-${tractOwnerships.id}`}>
+                <td id={`mineralInterest-${tractOwnerships.id}`}>
+                  <InputGroup className="owner">
+                    <FormControl placeholder={tractOwnerships.owner} />
+                  </InputGroup>
+                </td>
+                <td key={`mineralInterest-${tractOwnerships.id}`}>
+                  <InputGroup className="interest" id={tractOwnerships.id}>
+                    <FormControl placeholder={tractOwnerships.interest} />
+                    <InputGroup.Append>
+                      <InputGroup.Text>%</InputGroup.Text>
+                    </InputGroup.Append>
+                  </InputGroup>
+                </td>
+                <td id={`mineralinterest-${tractOwnerships.id}`}></td>
+                <td id={`mineralinterest-${tractOwnerships.id}`}>
+                  <InputGroup className="lease">
+                    <FormControl placeholder={tractOwnerships.lease} />
+                  </InputGroup>
+                </td>
+                <td>
+                  <Icon icon={'remove'} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <div>
+        <Button variant="light">
+          {' '}
+          <Icon icon={'add'} /> Add NPRI{' '}
+        </Button>
+      </div>
+      <div>
+        <Button variant="light">
+          {' '}
+          <Icon icon={'add'} /> Add Mineral Interest{' '}
+        </Button>
+      </div>
+    </div>
   );
 };
 
